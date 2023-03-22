8e12951cbab363ca447fc83db5031a9eed25ead3
FunTazz0001 <106074252+FunTazz0001@users.noreply.github.com>
2023-03-22 13:48:38 +0100

    Test de base de donnée (création, insertion, et lecture des données sur le fichier SQLiteTest.cs

--- Assets/Script/SqliteTest.cs
+++ Assets/Script/SqliteTest.cs
@@ -1,13 +1,50 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Data;
+using Mono.Data.Sqlite;
+using System.IO;
 
 public class SqliteTest : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // A utiliser pour l'initialisation
     void Start()
     {
-        
+        // Création de la base de donnée
+        string connection = "URI=file:" + Application.persistentDataPath + "/My_Database"; 
+
+        // Ouverture de la connection avec la base de donnée
+        IDbConnection dbcon = new SqliteConnection(connection);
+        dbcon.Open();
+
+        // Création d'une table
+        IDbCommand dbcmd;
+        IDataReader reader;
+
+        dbcmd = dbcon.CreateCommand();
+        string q_createTable =
+          "CREATE TABLE IF NOT EXISTS my_table (id INTEGER PRIMARY KEY, val INTEGER )";
+
+        dbcmd.CommandText = q_createTable;
+        reader = dbcmd.ExecuteReader();
+
+        // Insertion de valeur dans la table
+        IDbCommand cmnd = dbcon.CreateCommand();
+        cmnd.CommandText = "INSERT INTO my_table (id, val) VALUES (0, 5)";
+        cmnd.ExecuteNonQuery();
+
+        // Lire et imprimer toutes les valeurs du tableau
+        IDbCommand cmnd_read = dbcon.CreateCommand();
+        IDataReader reader;
+        string query = "SELECT * FROM my_table";
+        cmnd_read.CommandText = query;
+        reader = cmnd_read.ExecuteReader();
+
+        while (reader.Read())
+        {
+            Debug.Log("id: " + reader[0].ToString());
+            Debug.Log("val: " + reader[1].ToString());
+        }
+
+        dbcon.Close();
     }
 
     // Update is called once per frame
