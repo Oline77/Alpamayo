37aab9485f49ae42ae7c33443014a2d88b0acfdc
FunTazz0001 <106074252+FunTazz0001@users.noreply.github.com>
2023-03-23 08:23:50 +0100

    Ajout de commentaire au script SqliteHelper.cs

--- Assets/Script/SqliteHelper.cs
+++ Assets/Script/SqliteHelper.cs
@@ -12,11 +12,16 @@ namespace DataBank
     {
         private const string Tag = "Riz: SqliteHelper:\t";
 
+        // Nom de la base de données
         private const string database_name = "my_db";
 
+        // Chaine de connection à la base de données
         public string db_connection_string;
+
+        // Connexion à la base de données
         public IDbConnection db_connection;
 
+        // Constructeur de la classe, initialise la connexion à la base de données
         public SqliteHelper()
         {
             db_connection_string = "URI=file:" + Application.persistentDataPath + "/" + database_name;
@@ -25,12 +30,13 @@ namespace DataBank
             db_connection.Open();
         }
 
+        // Destructeur de la classe, ferme la connexion à la base de données
         ~SqliteHelper()
         {
             db_connection.Close();
         }
 
-        // virtual functions
+        // Fonctions virtuelles (à implémenter dans les classes qui héritent de cette classe)
         public virtual IDataReader getDataById(int id)
         {
             Debug.Log(Tag + "This function is not implemnted");
@@ -73,12 +79,14 @@ namespace DataBank
             throw null;
         }
 
-        //helper functions
+        // Fonctions d'aide
+        // Retourne un objet IDbCommand pour exécuter des requêtes SQL sur la base de données
         public IDbCommand getDbCommand()
         {
             return db_connection.CreateCommand();
         }
 
+        // Renvoie tous les enregistrements de la table spécifiée
         public IDataReader getAllData(string table_name)
         {
             IDbCommand dbcmd = db_connection.CreateCommand();
@@ -88,6 +96,7 @@ namespace DataBank
             return reader;
         }
 
+        // Supprime toutes les données de la table spécifiée
         public void deleteAllData(string table_name)
         {
             IDbCommand dbcmd = db_connection.CreateCommand();
@@ -95,6 +104,7 @@ namespace DataBank
             dbcmd.ExecuteNonQuery();
         }
 
+        // Renvoie le nombre total de lignes dans la table spécifiée
         public IDataReader getNumOfRows(string table_name)
         {
             IDbCommand dbcmd = db_connection.CreateCommand();
