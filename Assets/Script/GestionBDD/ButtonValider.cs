eb11196a45af3a06d7f32d34020f6000cbb84a33
FunTazz0001 <106074252+FunTazz0001@users.noreply.github.com>
2023-04-05 16:32:46 +0200

    refactor(Bouton Valider Creation Projet) : Mise en place de la bibliothèque TMPro
    Les valeurs int ne peuvent pas encore être convertit, je les ai donc mis en commentaire pour éviter de pull du code comportant des erreurs.

--- Assets/Script/GestionBDD/ButtonValider.cs
+++ Assets/Script/GestionBDD/ButtonValider.cs
@@ -2,7 +2,7 @@
 /// @brief: Permet que lorsqu'on click sur le bouton valider, les informations du formulaire sont envoyé dans la class DatabaseHelper pour être traité
 /// @author: Barbaud M.
 /// @date: 04/04/2023
-/// @update: ~
+/// @update: 05/04/2023
 
 using System.Collections;
 using System.Collections.Generic;
@@ -14,37 +14,56 @@ using TMPro;
 public class ButtonValider : MonoBehaviour
 {
     // Déclaration des champs de saisie dans l'interface graphique de l'utilisateur
-    public InputField nomProjetInput;
-    public InputField nomClientInput;
-    public InputField numeroChantierInput;
-    public InputField adresseInput;
-    public InputField voieInput;
-    public InputField codePostalInput;
-    public InputField villeInput;
-    public InputField descriptionInput;
+    public TextMeshProUGUI nomProjetInput;
+    public TextMeshProUGUI nomClientInput;
+    public TextMeshProUGUI numeroChantierInput;
+    public TextMeshProUGUI adresseInput;
+    public TextMeshProUGUI voieInput;
+    public TextMeshProUGUI codePostalInput;
+    public TextMeshProUGUI villeInput;
+    public TextMeshProUGUI descriptionInput;
 
     // Fonction qui est appelée lorsque l'utilisateur clique sur le bouton Valider
     public void OnValiderButtonClick()
     {
+        /*
+        // Vérifier que le champ "numeroChantierInput" contient un nombre valide;
+        if (!int.TryParse(numeroChantierInput.text, out numeroChantier))
+        {
+            Debug.LogError("Le champ Numero Chantier doit contenir un nombre valide.");
+        }
+
+        // Vérifier que le champ "voieInput" contient un nombre valide
+        if (!int.TryParse(voieInput.text, out voie))
+        {
+            Debug.LogError("Le champ Voie doit contenir un nombre valide.");
+        }
+
+        // Vérifier que le champ "codePostalInput" contient un nombre valide
+        if (!int.TryParse(codePostalInput.text, out codePostal))
+        {
+            Debug.LogError("Le champ Code Postal doit contenir un nombre valide.");
+        }*/
+
         Project newProject = new Project();
 
         // Attribution des valeurs entrées par l'utilisateur aux propriétés de l'objet Project
         newProject.NomProjet = nomProjetInput.text;
         newProject.NomClient = nomClientInput.text;
-        newProject.NumeroChantier = int.Parse(numeroChantierInput.text);
+        //newProject.NumeroChantier = int.Parse(numeroChantierInput.text);
         newProject.Adresse = adresseInput.text;
-        newProject.Voie = int.Parse(voieInput.text);
-        newProject.CodePostale = int.Parse(codePostalInput.text);
+        //newProject.Voie = int.Parse(voieInput.text);
+        //newProject.CodePostale = int.Parse(codePostalInput.text);
         newProject.Ville = villeInput.text;
         newProject.Description = descriptionInput.text;
 
         try
         {
             // Création d'un objet helper pour interagir avec la base de données
-            DatabaseHelper helper = new DatabaseHelper();
+            DatabaseHelper databaseHelper = new DatabaseHelper();
 
             // Insertion des données pour être traitées
-            helper.InsertProject(newProject);
+            databaseHelper.InsertProject(newProject);
         }
         catch (System.Exception e)
         {
