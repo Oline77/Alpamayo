7dc83b042fe79b6e37dc23c5d3e3a3a47789c478
NicoCHR1 <122549023+NicoCHR1@users.noreply.github.com>
2023-05-04 17:14:25 +0200

    Selection Objet Fixe
    Tout fonctionne seul problème c'est avec l'interface il faut mettre l'objet sélectionné sous le bouton supprimé (se problème on l'a sur toute l'interface)

--- Assets/Script/ObjectSelection.cs
+++ Assets/Script/ObjectSelection.cs
@@ -2,98 +2,98 @@ using UnityEngine;
 
 public class ObjectSelection : MonoBehaviour
 {
-    // Tag utilisé pour identifier les objets sélectionnables
-    public string selectableTag = "Selectable";
+    public static GameObject selectedObject; // Variable statique pour stocker l'objet sélectionné
+    private bool isDragging = false;
+    private Vector3 touchOffset;
+    public GameObject cameraEmpty; // Référence à l'objet vide contenant le script de déplacement de la caméra
+    public GameObject selectionButton; // Référence à l'objet du bouton de sélection
+   
+    public Material selectionMaterial;
 
-    // Couleur de l'objet lorsqu'il est sélectionné
-    public Color highlightColor = Color.yellow;
+    private Renderer selectedRenderer;
+    private Material originalMaterial;
 
-    // Couleur par défaut de l'objet
-    private Color defaultColor;
-
-    // Stocke l'objet actuellement sélectionné
-    private Transform selectedObject;
-
-    public Zoom cameraZoom; // Référence au script Zoom de la caméra
-
-    void Start()
+    private void Update()
     {
-        cameraZoom = Camera.main.GetComponent<Zoom>();
-        // Récupère la couleur par défaut de l'objet
-        defaultColor = GetComponent<Renderer>().material.color;
-    }
-
-    void Update()
-    {
-        // Vérifie si un toucher est détecté
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
-            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
 
-            // Vérifie si l'utilisateur a appuyé sur l'écran
             if (touch.phase == TouchPhase.Began)
             {
-                // Cast un rayon à partir de la caméra vers la position du toucher pour détecter les objets sélectionnables
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
                 RaycastHit hit;
-                if (Physics.Raycast(touchPosition, Camera.main.transform.forward, out hit))
-                {
-                    // Vérifie si l'objet détecté a le tag "selectable"
-                    if (hit.collider.CompareTag(selectableTag))
-                    {
-                        // Si un objet était déjà sélectionné, le désélectionne
-                        if (selectedObject != null && selectedObject != hit.transform)
-                        {
-                            // Remettre l'objet précédemment sélectionné à sa couleur par défaut
-                            selectedObject.GetComponent<Renderer>().material.color = defaultColor;
-                        }
 
-                        // Stocke l'objet sélectionné
-                        selectedObject = hit.transform;
-
-                        // Change la couleur de l'objet sélectionné
-                        selectedObject.GetComponent<Renderer>().material.color = highlightColor;
-                        // Désactiver le script Zoom de la caméra
-                        cameraZoom.enabled = false;
-                    }
-                }
-            }
-            else if (touch.phase == TouchPhase.Moved)
-            {
-                // Vérifie si un objet est sélectionné
                 if (selectedObject != null)
                 {
-                    // Vérifie si le toucher se produit directement sur l'objet sélectionné
-                    Ray ray = Camera.main.ScreenPointToRay(touch.position);
-                    RaycastHit hit;
-                    if (Physics.Raycast(ray, out hit) && hit.transform == selectedObject)
+                    // Si l'objet est déjà sélectionné et que le raycast ne touche pas l'objet, le désélectionner
+                    if (!Physics.Raycast(ray, out hit) || hit.collider.gameObject != selectedObject)
                     {
-                        // Convertit la position de l'écran en rayon à partir de la caméra
-                        ray = Camera.main.ScreenPointToRay(touch.position);
-
-                        // Calcule la distance le long du rayon où l'objet doit être déplacé
-                        float distanceAlongRay = (selectedObject.position - ray.origin).magnitude;
-
-                        // Calcule la nouvelle position en utilisant le rayon et la distance calculée
-                        Vector3 newPosition = ray.GetPoint(distanceAlongRay);
-
-                        // Déplace l'objet à la nouvelle position
-                        selectedObject.position = newPosition;
-                        selectedObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                        DeselectObject(selectedObject);
+                        selectedObject = null;
+                        return;
                     }
                 }
-            }
 
-            else if (touch.phase == TouchPhase.Ended)
-            {
-                // Désélectionne l'objet lorsque le toucher se termine
-                if (selectedObject != null)
+                if (Physics.Raycast(ray, out hit) && hit.collider.CompareTag("Selectable"))
                 {
-                    selectedObject.GetComponent<Renderer>().material.color = defaultColor;
-                    selectedObject = null;
-                    cameraZoom.enabled = true;
+                    // Sélectionner l'objet si le raycast touche un objet avec le tag "Selectable"
+                    selectedObject = hit.collider.gameObject;
+                    SelectObject(selectedObject);
+                    touchOffset = hit.point - selectedObject.transform.position;
+                    isDragging = true;
                 }
+            }
+            else if (touch.phase == TouchPhase.Moved && isDragging)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    selectedObject.transform.position = hit.point - touchOffset;
+                }
+            }
+            else if (touch.phase == TouchPhase.Ended && isDragging)
+            {
+                isDragging = false;
             }
         }
+    }
+
+    private void SelectObject(GameObject obj)
+    {
+        cameraEmpty.SetActive(false);
+        selectionButton.SetActive(true);
+
+        // Stocker le matériau d'origine de l'objet sélectionné
+        selectedRenderer = obj.GetComponent<Renderer>();
+        if (selectedRenderer != null)
+        {
+            originalMaterial = selectedRenderer.material;
+        }
+
+        // Appliquer le matériau de sélection à l'objet
+        if (selectedRenderer != null)
+        {
+            selectedRenderer.material = selectionMaterial;
+        }
+    }
+
+    public void DeselectObject(GameObject obj)
+    {
+        cameraEmpty.SetActive(true);
+        selectionButton.SetActive(false);
+
+        // Restaurer le matériau d'origine de l'objet désélectionné
+        if (selectedRenderer != null)
+        {
+            selectedRenderer.material = originalMaterial;
+        }
+
+        // Réinitialiser les variables de sélection
+        selectedObject = null;
+        selectedRenderer = null;
+        originalMaterial = null;
     }
 }
